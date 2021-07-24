db05fae314e8d7a680ac3d154aaab26702877f80
muhittinpalamutcu <muhittinpalamutcu@gmail.com>
2021-07-24 17:17:00 +0300

    Routing and screens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import SearchScreen from "./Screens/SearchScreen";
 
-function App() {
-  return <div className="">spotify-ui-clone</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={HomeScreen} exact />
+        <Route path="/search" component={SearchScreen} />
+        <Route path="/library" />
+        <Route path="/playlist/:listname" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/Screens/HomeScreen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="text-9xl">Home Screen</div>;
+};
+
+export default Home;
